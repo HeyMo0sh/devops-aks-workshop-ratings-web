4a6dec3114e6e7a0d71f3a813e63d91d36cf6dde
HeyMo0sh <hamishwatson8@outlook.com>
2020-06-07 21:11:17 +1200

    Updated file

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        (AKS) workshop
+        Hamish Watson (AKS) workshop
       </div>
       <div class="col-lg-6">
       </div>
