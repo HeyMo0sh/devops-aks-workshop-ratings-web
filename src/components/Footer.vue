667854c2235a70d59d65e21717a8c4ae68d8d219
HamishWatson8 <hamishWatson8@outlook.com>
2020-06-11 09:27:09 +1200

    Made a change

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-       Hamish Watson's (AKS) workshop
+       Callaghan Innovation DevOps Workshop
       </div>
       <div class="col-lg-6">
       </div>
